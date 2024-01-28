7d89cb15114202617b2b9c72ed9c7709ba8f7a47
Julien ARNOULD <ju.arnould@gmail.com>
2024-01-28 16:09:55 +0100

    Exercise 3.11

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 const app = express()
-
 app.use(cors())
+
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
